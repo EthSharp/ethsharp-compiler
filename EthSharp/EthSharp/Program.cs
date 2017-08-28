4d1dab026cd688e613832b3542a8ecc2e8ae21a7
Jandrews <jordandrews@hotmail.com>
2017-08-28 10:03:44 -0500

    EthSharp Begins

---
+++ EthSharp/EthSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"public class SimpleStorage {
+    private int storedData;
+
+    public void Set(int x) {
+        storedData = x;
+    }
+
+    public int Get(){
+        return storedData;
+    }
+}";
+            //parse tree - create assembly then emit.
+
+            var tree = SyntaxFactory.ParseSyntaxTree(source);
+            var assembly = new EthSharpCompiler().Create(tree);
+            Console.ReadKey();
+        }
+    }
+}
