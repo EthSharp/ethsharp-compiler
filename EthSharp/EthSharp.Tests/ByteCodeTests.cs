f41d9bc9f29da038c1a65c81e8f51fc08e418868
Jandrews <jordandrews@hotmail.com>
2017-09-08 16:07:11 -0500

    Start on Testing EVM

---
+++ EthSharp/EthSharp.Tests/ByteCodeTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EthSharp.Tests.VM;
+using Xunit;
+
+namespace EthSharp.Tests
+{
+    public class ByteCodeTests
+    {
+        public ByteCodeTests()
+        {
+            
+        }
+
+        [Fact]
+        public void TestSomething()
+        {
+            Console.WriteLine("test");
+        }
+
+    }
+}
---
+++ EthSharp/EthSharp.Tests/EVM/EvmProgram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using EthSharp;
+using EthSharp.Compiler;
+using EthSharp.ContractDevelopment;
+
+namespace EthSharp.Tests.VM
+{
+    public class EvmProgram
+    {
+        public Stack<object> Stack { get; set; }
+        public bool Stopped { get; set; }
+        public IList<byte> ByteCode { get; private set; }
+        public int Position { get; set; }
+
+        public EvmProgram(IList<byte> byteCode)
+        {
+            ByteCode = byteCode;
+        }
+
+        public void Run()
+        {
+            while (!Stopped)
+            {
+                MakeStep();
+            }
+        }
+
+        public void MakeStep()
+        {
+            switch ((EvmInstruction)ByteCode[Position])
+            {
+                case EvmInstruction.ADD:
+                    Stack.Push((UInt256)Stack.Pop() + (UInt256) Stack.Pop());
+                    break;
+                default:
+                    throw new Exception("Invalid Opcode");
+            }
+        }
+    }
+}
