c4fc681b986698238c70ae760951c31cb98a6a44
Rudy Alvarez <rudygt@gmail.com>
2017-09-15 00:09:52 -0600

    added tests for allowed types visitor, small refactor to the allowed types visitor

--- EthSharp/EthSharp/Compiler/EthSharpAllowedTypesVisitor.cs
+++ EthSharp/EthSharp/Compiler/EthSharpAllowedTypesVisitor.cs
@@ -6,60 +6,64 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace EthSharp.Compiler
 {
-    public class EthSharpAllowedTypesVisitor : CSharpSyntaxVisitor
+    public class EthSharpAllowedTypesVisitor : CSharpSyntaxWalker
     {
-        private static readonly HashSet<string> AllowedTypes = new HashSet<string>
+        private static HashSet<string> AllowedTypes = new HashSet<string>
         {
             typeof(UInt256).Name
         };
 
         private string ExceptionMessage => "Type not supported. Currently supported types: " + String.Join(", ", AllowedTypes);
 
-        private bool TypeAllowed(TypeSyntax type)
-        {
-            return AllowedTypes.Contains(type.GetText().ToString().Trim());
+        public EthSharpAllowedTypesVisitor()
+        {            
         }
 
-
-        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        public EthSharpAllowedTypesVisitor(HashSet<string> allowedTypes)
         {
-            foreach (var property in node.GetProperties())
+            AllowedTypes = allowedTypes;
+        }
+
+        private bool TypeAllowed(string typeName)
+        {
+            return AllowedTypes.Contains(typeName);
+        }
+
+        private void CheckIfTypeIsAllowed(TypeSyntax type)
+        {
+            var typeName = type.GetText().ToString().Trim();
+
+            if (!TypeAllowed(typeName))
             {
-                if (!TypeAllowed(property.Type))
-                    throw new NotImplementedException(ExceptionMessage); // TODO: Make these errors explicit
+                throw new Exception("[" + typeName + "] " + ExceptionMessage); // TODO: Make these errors explicit
             }
-            foreach (var field in node.GetFields())
-            {
-                if (!TypeAllowed(field.Declaration.Type))
-                    throw new NotImplementedException(ExceptionMessage);
-            }
-            foreach (var method in node.GetMethods())
-            {
-                method.Accept(this);
-            }
+        }
+        
+        public override void VisitFieldDeclaration(FieldDeclarationSyntax node)
+        {
+            CheckIfTypeIsAllowed(node.Declaration.Type);
+        }
+
+        public override void VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            CheckIfTypeIsAllowed(node.Type);
+            base.VisitPropertyDeclaration(node);
+        }
+
+        public override void VisitParameter(ParameterSyntax node)
+        {
+            CheckIfTypeIsAllowed(node.Type);
         }
 
         public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
         {
-            node.ParameterList.Accept(this);
-            node.Body.Accept(this);
+            CheckIfTypeIsAllowed(node.ReturnType);
+            base.VisitMethodDeclaration(node);
         }
 
-        public override void VisitParameterList(ParameterListSyntax node)
+        public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
         {
-            foreach (var parameter in node.Parameters)
-            {
-                if (!TypeAllowed(parameter.Type))
-                    throw new NotImplementedException(ExceptionMessage);
-            }
-        }
-
-        public override void VisitBlock(BlockSyntax node)
-        {
-            foreach (var statement in node.Statements)
-            {
-                statement.Accept(this);
-            }
+            CheckIfTypeIsAllowed(node.Declaration.Type);            
         }
     }
 }
